faee3ba39d2446addbde6b04362a70b7eea17e9c
Robin Spahn <rspahn@alumni.albany.edu>
2025-10-30 23:54:24 +0000

    add that logo, not a modified version

--- src/components/layout/grifkuba-logo.tsx
+++ src/components/layout/grifkuba-logo.tsx
@@ -5,45 +5,18 @@ export function GrifkubaLogo(props: SVGProps<SVGSVGElement>) {
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 200 200"
+      viewBox="0 0 100 100"
+      fill="none"
     >
-      <defs>
-        <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
-          <feGaussianBlur stdDeviation="5" result="coloredBlur" />
-          <feMerge>
-            <feMergeNode in="coloredBlur" />
-            <feMergeNode in="SourceGraphic" />
-          </feMerge>
-        </filter>
-        <pattern id="bricks" patternUnits="userSpaceOnUse" width="40" height="20">
-            <rect width="40" height="20" fill="#7d342b"></rect>
-            <path d="M 40 0 L 40 20 M 20 10 L 40 10 M 0 10 L 20 10 M 20 0 L 20 10" stroke="#57241e" strokeWidth="1"></path>
-        </pattern>
-      </defs>
-
-      <rect width="200" height="200" fill="url(#bricks)" />
-      <rect width="200" height="200" fill="black" opacity="0.4" />
-      
-      <text
-        x="50%"
-        y="55%"
-        fontFamily="Arial, sans-serif"
-        fontSize="34"
-        fontWeight="bold"
-        fill="white"
-        textAnchor="middle"
-        dominantBaseline="middle"
-        filter="url(#glow)"
-        style={{
-          transform: 'skewX(-10deg)',
-          letterSpacing: '-1px',
-          paintOrder: 'stroke',
-          stroke: '#ccc',
-          strokeWidth: '0.5px'
-        }}
-      >
-        grifkuba
-      </text>
+      <path
+        d="M85.41 12.3H14.59C13.52 12.3 12.63 13.18 12.63 14.24V57.6C12.63 64.9 16.52 73.17 23.36 78.3L46.86 96.51C48.59 97.83 51.41 97.83 53.14 96.51L76.64 78.3C83.48 73.17 87.37 64.9 87.37 57.6V14.24C87.37 13.18 86.48 12.3 85.41 12.3Z"
+        className="text-primary"
+        fill="currentColor"
+      />
+      <path
+        d="M68.79 56.43C68.79 63.63 63.35 69.41 56.62 69.41H43.38C36.65 69.41 31.21 63.63 31.21 56.43V43.57C31.21 36.37 36.65 30.59 43.38 30.59H51.58V40.7H43.38C42.13 40.7 41.12 41.8 41.12 43.23V56.77C41.12 58.2 42.13 59.3 43.38 59.3H56.62C57.87 59.3 58.88 58.2 58.88 56.77V48.64H68.79V56.43Z"
+        fill="hsl(var(--background))"
+      />
     </svg>
   );
 }
